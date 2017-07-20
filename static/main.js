0b34135df022a7416bdeb95395573dfccb06c5b7
prakharchoudhary <prakhar2397@gmail.com>
2017-07-20 21:39:06 +0530

    use javascript to toggle the check button

--- static/main.js
+++ static/main.js
@@ -1,8 +1,38 @@
-$('#check').click(function() {
+$('.check_button').click(function() {
 	console.log("Clicked!");
+	var c = $(this).attr('class').split(" ");
+	console.log(c);
+	if(c[1] == 'notDone'){
+		$(this).removeClass('notDone');
+		$(this).addClass('Done');
+		$(this).text('Uncheck');
+		$('#task').addClass('taskdone')
+	}
+	else if(c[1] == 'Done'){
+		$(this).removeClass('Done');
+		$(this).addClass('notDone');
+		$(this).text('Check');
+		$('#task').removeClass('taskdone')
+	}
 });
 
 
 $( document ).ready(function() {
     console.log( "ready!" );
+    /*
+    Assings a class to the check button on the basis of status;
+    as the page initializes.
+    */
+    $('.check_button').each(function() {
+    	if("{{ i.is_Done }}" == true){
+    		$(this).addClass('Done');
+			$(this).text('Uncheck');
+			$('#task').addClass('taskdone')
+
+    	}
+    	else
+    		$(this).addClass('notDone');
+			$(this).text('Check');
+			$('#task').removeClass('taskdone')
+    });
 });
